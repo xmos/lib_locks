9bdc461ba061d4c0686887727e8a8f87f0dd5ace
Daniel Pieczko <danielpieczko@xmos.com>
2024-09-05 16:13:37 +0100

    Convert tests to XCommon CMake

--- tests/test_lib_locks.py
+++ tests/test_lib_locks.py
@@ -6,36 +6,30 @@ Runs tests for both hardware and software locks
 from subprocess import run
 from pathlib import Path
 import pytest
-import os
-import shutil
 
 
-@pytest.fixture(scope="session", params=("XCORE-AI-EXPLORER", "XCORE-200-EXPLORER"))
-def target(request):
-    this_target = request.param
-    cwd = Path(__file__).parent / "locks_test"
-    xmake_env = dict(**os.environ)
-    xmake_env["TARGET"] = this_target
-    run(["xmake"], check=True, env=xmake_env, cwd=cwd)
-
-    # need to delete .build_* dirs when using multiple targets
-    for d in cwd.glob(".build_*"):
-        shutil.rmtree(d)
-    return this_target
+@pytest.fixture(scope="session")
+def cmake_configure():
+    cwd = Path(__file__).parent
+    run(["cmake", "-B", "build", "-G", "Unix Makefiles"], check=True)
 
 
 @pytest.mark.parametrize("lock_type", ("HW", "SW"))
-def test_lib_locks(lock_type, target):
+@pytest.mark.parametrize("target", ("xs2", "xs3"))
+def test_lib_locks(cmake_configure, lock_type, target):
+    cwd = Path(__file__). parent / "build"
+    run(["xmake", f"locks_test_{target}_{lock_type}"], check=True, cwd=cwd)
+
     expected_output = (Path(__file__).parent / "locks.expect").read_text().strip()
     xe_path = (
         Path(__file__).parent
-        / "locks_test"
+        / f"locks_test_{target}"
         / "bin"
         / lock_type
-        / f"{target}_{lock_type}.xe"
+        / f"locks_test_{target}_{lock_type}.xe"
     )
-    res = run(["xsim", str(xe_path)], capture_output=True, text=True, check=True)
 
+    res = run(["xsim", xe_path], capture_output=True, text=True, check=True)
     assert (
         expected_output == res.stdout.strip()
     ), "Output of app did not match expected result"
